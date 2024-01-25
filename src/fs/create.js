3123d7ea3997afa64fbfd28935559db746f379fb
Alex Tokarev <aliaksei.tokarev@gmail.com>
2024-01-25 11:25:40 +0100

    feat: implement create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { appendFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const dirName = fileURLToPath(new URL('.', import.meta.url)),
+          foldeName = 'files',
+          fileName = 'fresh.txt',
+          content = 'I am fresh and young',
+          fullPath = path.join(dirName, foldeName, fileName);
+
+    try {
+       await appendFile(fullPath, content, {flag: 'ax'})
+    } catch (error) {
+       if (error.code === 'EEXIST') throw new Error('FS operation failed');
+       else throw error;
+    }
 };
 
 await create();
