beff5d58b87d139d45c5ebdebf66765853c36d71
Alex Tokarev <aliaksei.tokarev@gmail.com>
2024-01-28 21:03:41 +0100

    feat: implement env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    let result = '';
+    const perfix = 'RSS_';
+    for (const entire in env) {
+        if (entire.includes(perfix))
+        result += `${entire}=${env[entire]}; `;
+    }
+    console.log(result.trimEnd().slice(0, result.length-2));
 };
 
 parseEnv();
