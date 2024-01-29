baaa3233421a28c0e206d4274d6f9ea1b65e19bc
Alex Tokarev <aliaksei.tokarev@gmail.com>
2024-01-29 14:25:20 +0100

    feat: implement read stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'node:fs';
+// import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+import path, { resolve } from 'node:path'
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+    const dirName = fileURLToPath(new URL('.', import.meta.url)),
+          folderName = 'files',
+          fileToRead = 'fileToRead.txt',
+          filePath = path.join(dirName, folderName, fileToRead);
+
+    const fileStream = createReadStream(filePath);
+        
+    for await (const chunk of fileStream) {
+        stdout.write(chunk);
+    }
+    
+    fileStream.close();
 };
 
 await read();
